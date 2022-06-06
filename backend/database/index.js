d65e4cb7f7ac0ad0eb4f077d0948b129747a780b
jeremy lorette <jeremy.lorette@outlook.com>
2022-06-06 15:24:08 +0200

    backend folder + i express , i mongoose , i path , co mongoDB ok

---
+++ backend/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Wormav:qwe@cluster0.tl7kw.mongodb.net/Piiquante?retryWrites=true&w=majority')
+                .then(() => console.log('connexion DB ok!'))
+                .catch(err => console.log(err))
+
